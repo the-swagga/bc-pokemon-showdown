607cb463c7c38fe52eab34f240312b7fb6ddcd10
the-swagga <144693488+the-swagga@users.noreply.github.com>
2026-03-17 01:03:45 +0000

    Add loop to iterate over 1000 seeds

--- main.py
+++ main.py
@@ -7,9 +7,28 @@ from src.model.quant_eval import quantitative_evaluate_model
 def preprocess_train_evaluate(seed=38, records=0, show_loss=True):
     preprocess_data(seed=seed, records=records)
     train_model(seed=seed, show_loss=show_loss)
-    quantitative_evaluate_model()
+    acc = quantitative_evaluate_model()
     print()
+    return acc
 
-preprocess_train_evaluate(seed=38, show_loss=False)
+acc = 0
+top3acc = 0
+cum_acc = 0
+cum_top3acc = 0
+best_acc = -1
+best_seed = -1
 
+#for i in range(1, 1001):
+    #acc, top3acc = preprocess_train_evaluate(seed=i, records=1776, show_loss=False)
+    #cum_acc += acc
+    #cum_top3acc += top3acc
 
+    #if acc > best_acc:
+        #best_acc = acc
+        #best_seed = i
+
+#print(f"Average Accuracy: {cum_acc/1000}")
+#print(f"Average Top 3 Accuracy: {cum_top3acc/1000}")
+#print(f"Best Seed by Average Accuracy: {best_seed}")
+
+preprocess_train_evaluate(seed=173, records=1776, show_loss=True)
